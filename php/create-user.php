e06f513ba4d3ba5830c62ba75e5237a3d0c83e8b
harrison8024 <harrison8024@gmail.com>
2018-11-19 18:45:25 -0800

    add axios call to create user

--- php/create-user.php
+++ php/create-user.php
@@ -1,16 +1,17 @@
 <?php
 require('./cross-origin.php');
 require('./header.php');
-
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata,true);
 
 $stmt = $conn->prepare("INSERT INTO `user` (`id`, `username`, `firstname`, `lastname`, `hashpassword`, `email`, `token`, `privilege`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)");
 $stmt -> bind_param("ssssssi", $username, $firstname, $lastname, $hashpassword, $email, $token, $privilege);
 
-$username = "test";
-$firstname = "test";
-$lastname = "test";
+$username = $request['username'];
+$firstname = $request['firstname'];
+$lastname = $request['lastname'];
 $hashpassword = password_hash("test", PASSWORD_DEFAULT);
-$email = "test";
+$email = $request['email'];
 $token = "test";
 $privilege = 0;
 
