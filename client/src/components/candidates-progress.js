c40bad0441ef7badffa1f7e21c870d3e361a29c7
ddonjuan <daniel.donjuan25@gmail.com>
2018-11-23 22:34:56 -0800

    finished adding progress page buttons and modals with dummy data

--- client/src/components/candidates-progress.js
+++ client/src/components/candidates-progress.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Modal from './helpers/modal';
+import StudentModal from './helpers/student-modal';
 import { showElement } from './helpers/handle-input-helper';
 
 import dummyData from './dummy-data';
@@ -8,46 +8,89 @@ class CandidateProgress extends Component{
     constructor(props){
         super(props);
         this.state = {
-            candidate: []
+            candidate: [],
+            firstInterview: [],
+            secondInterview: [],
+            acceptedCandidates: []
         }
-        this.showCandidateButton = this.showCandidateButton.bind(this);
+        this.candidatesToScreen = this.candidatesToScreen.bind(this);
     }
     componentWillMount(){
         this.props.switchNav();
         this.props.hideDropDown();
     }
-    showCandidateButton(item, index){
+    candidatesToScreen(item, index){
         this.setState({
             candidate: item
         });
-        showElement("candidate-info");
+        showElement("candidates-to-screen");
     }
+    sendCandidatesToFirstInterview(item, index){
+        this.setState({
+            firstInterview: item
+        });
+        showElement("first-interview");
+    }
+    sendCandidatesToSecondInterview(item, index){
+        this.setState({
+            secondInterview: item
+        });
+        showElement("second-interview");
+    }
+    acceptedCandidates(item, index){
+        this.setState({
+            acceptedCandidates: item
+        });
+        showElement("accepted-candidates");
+    }
+    //axio call 
+    //make sure to send in the ID that will grab the candidate by ID and change the status
 
     render(){
-        console.log("this is the state: ", this.state.candidate);
-        const {candidate} = this.state;
+        const {candidate, firstInterview, secondInterview, acceptedCandidates} = this.state;
         const {firstname, lastname, essay1, essay2, school, department} = candidate;
         const candidates = dummyData.map((item, index)=>{
             const {firstname, lastname, id} = item;
             return(
-                <button data={item} onClick={()=>{this.showCandidateButton(item)}} className="waves-effect waves-light orange btn candidate-button-progress">{`${firstname} ${lastname}`}</button>
+                <button data={item} onClick={()=>{this.candidatesToScreen(item)}} className="waves-effect waves-light orange btn candidate-button-progress">{`${firstname} ${lastname}`}</button>
+            )
+        })
+        const candidatesRound1 = dummyData.map((item, index)=>{
+            const {firstname, lastname, id} = item;
+            return(
+                <button data={item} onClick={()=>{this.sendCandidatesToFirstInterview(item)}} className="waves-effect waves-light blue btn candidate-button-progress">{`${firstname} ${lastname}`}</button>
+            )
+        })
+        const candidatesRound2 = dummyData.map((item, index)=>{
+            const {firstname, lastname, id} = item;
+            return(
+                <button data={item} onClick={()=>{this.sendCandidatesToSecondInterview(item)}} className="waves-effect waves-light blue btn candidate-button-progress">{`${firstname} ${lastname}`}</button>
+            )
+        })
+        const acceptedCandidatesFinal = dummyData.map((item, index)=>{
+            const {firstname, lastname, id} = item;
+            return(
+                <button data={item} onClick={()=>{this.acceptedCandidates(item)}} className="waves-effect waves-light green btn candidate-button-progress">{`${firstname} ${lastname}`}</button>
             )
         })
         return(
             <div className="container swimming-lanes">
-            <Modal id="candidate-info" title={`${firstname} ${lastname}`}/>
+            <StudentModal id="candidates-to-screen" title={"Are you sure you want to send this candidate to the First Interview?"} name={`${firstname} ${lastname}`} department={department} school={school}/>
+            <StudentModal id="first-interview" title={"Are you sure you want to send this candidate to the Second Interview?"} name={`${firstInterview.firstname} ${firstInterview.lastname}`} department={firstInterview.department} school={firstInterview.school}/>
+            <StudentModal id="second-interview" title={"Do you want to hire this candidates?"} name={`${secondInterview.firstname} ${secondInterview.lastname}`} department={secondInterview.department} school={secondInterview.school}/>
+            <StudentModal id="accepted-candidates" title={"Hired"} name={`${acceptedCandidates.firstname} ${acceptedCandidates.lastname}`} department={acceptedCandidates.department} school={acceptedCandidates.school}/>
                 <div className="row">
                     <div className="col s12 lanes-title">
-                        <div className="col s3 lanes">Potential Employees</div>
-                        <div className="col s3 lanes">Interview 1</div>
-                        <div className="col s3 lanes">Interview 2</div>
-                        <div className="col s3 lanes">Hired</div>
+                        <div className="col s3 lanes">Potential Employees <div className="divider"></div> </div>
+                        <div className="col s3 lanes">Interview 1 <div className="divider"></div> </div>
+                        <div className="col s3 lanes">Interview 2 <div className="divider"></div> </div>
+                        <div className="col s3 lanes">Hired <div className="divider"></div> </div>
                     </div>
                     <div className="col s12 candidate-lanes">
                         <div className="col s3 lanes potential">{candidates}</div>
-                        <div className="col s3 lanes potential"></div>
-                        <div className="col s3 lanes potential"></div>
-                        <div className="col s3 lanes potential"></div>
+                        <div className="col s3 lanes potential">{candidatesRound1}</div>
+                        <div className="col s3 lanes potential">{candidatesRound2}</div>
+                        <div className="col s3 lanes potential">{acceptedCandidatesFinal}</div>
                     </div>
                 </div>
             </div>
