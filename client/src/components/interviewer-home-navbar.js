fccccbc826fe27d527196181ceb1f4a239f231f1
harrison8024 <harrison8024@gmail.com>
2018-12-27 21:07:29 -0800

    change nav color

--- client/src/components/interviewer-home-navbar.js
+++ client/src/components/interviewer-home-navbar.js
@@ -8,21 +8,24 @@ class InterviewHomeNavbar extends Component{
     }
     render(){
         return (
-            <nav className="blue header">
+            <nav className="header">
                 <div className="nav-wrapper">
-                    <Link to="/interviewer-homepage" className="home-nav-logo">INTRVWS</Link>
-    
-                    <ul className="right nav-items">
-                        <li>
-                            <Link to="interviewer-homepage">Candidates</Link>
-                        </li>
-                        <li>
-                            <Link to="candidate-progress">Potential Employees</Link>
-                        </li>
-                        <li>
-                            <div onClick={()=>{this.props.toggleDropDownNav()}} className="dropdown-trigger" data-target="dropdown1"><i className="material-icons icon">person</i></div>
-                        </li>
-                    </ul>
+                    <div class="nav-logo-container">
+                        <Link to="/interviewer-homepage" className="home-nav-logo">INTRVWS</Link>
+                    </div>
+                    <div className="nav-items">
+                        <div>
+                            <div className="nav-button">
+                                <Link to="interviewer-homepage">Candidates</Link>
+                            </div>
+                            <div className="nav-button">
+                                <Link to="candidate-progress">Potential Employees</Link>
+                            </div>
+                            <div className="nav-icon">
+                                <div onClick={()=>{this.props.toggleDropDownNav()}} className="dropdown-trigger" data-target="dropdown1"><i className="material-icons icon">person</i></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </nav>
         )
